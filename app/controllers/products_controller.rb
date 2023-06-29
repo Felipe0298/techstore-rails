f8f3fce04b4bde24bc5d907989228a280024c235
felipe <felipe02-98@hotmail.com>
2023-06-29 11:25:45 -0500

    Agregando categorias

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,20 @@
 class ProductsController < ApplicationController
+  
   def index
     @products = Product.all
+    @categories = Product.pluck(:category).uniq
+    @select_category = params[:category]
+  
+    if @select_category.present?
+      @products = Product.where(category: @select_category)
+    else
+      @products = Product.all
+    end
   end
 
   def show
     @product = Product.find(params[:id])
   end
 end
+
+
