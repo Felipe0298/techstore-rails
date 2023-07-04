a8767fc4d8a2b798ce595116a4e4f921a8575899
Felipe <felipe02-98@hotmail.com>
2023-07-04 16:10:26 -0500

    Agregando boton de carrito y cantidad de items en el cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    before_action :set_cart_products_count
+  
+    private
+  
+    def set_cart_products_count
+      @cart_products_count = session[:cart]&.length || 0
+    end
 end
+  
