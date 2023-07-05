5af88fadced5341646e1b13ce71a5b2364d34a8c
Felipe <felipe02-98@hotmail.com>
2023-07-05 10:40:36 -0500

    Actulizando categorias

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,16 +1,18 @@
 class ProductsController < ApplicationController
   
   def index
-    @products = Product.all
     @categories = Product.pluck(:category).uniq
     @select_category = params[:category]
-  
+    
     if @select_category.present?
       @products = Product.where(category: @select_category)
+      @title = "Todos nuestros productos #{@select_category}"
     else
       @products = Product.all
+      @title = "Todos nuestros productos"
     end
   end
+  
 
   def show
     @product = Product.find(params[:id])
