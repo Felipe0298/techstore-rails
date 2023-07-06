593261d43042248fab3e697ae3f92ca8062a29f3
Felipe <felipe02-98@hotmail.com>
2023-07-06 09:19:07 -0500

    Agregando validaciones y traducciones

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,6 @@
 class Order < ApplicationRecord
+    validates :full_name, presence: true
+    validates :phone_number, presence: true, length: { minimum: 10 }
+    validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
+  
